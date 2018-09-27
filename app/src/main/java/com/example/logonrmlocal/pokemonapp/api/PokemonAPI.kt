ab372c0c611400a59ed2ce9123b679684831f7a1
Lidiane <alencar.lidiane@gmail.com>
2018-09-27 20:31:34 -0300

    innitial

--- app/src/main/java/com/example/logonrmlocal/pokemonapp/api/PokemonAPI.kt
+++ app/src/main/java/com/example/logonrmlocal/pokemonapp/api/PokemonAPI.kt
@@ -1,9 +1,12 @@
 package com.example.logonrmlocal.pokemonapp.api
 
+import com.example.logonrmlocal.pokemonapp.model.Pokemon
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PokemonAPI{
 
-    @GET("")
-
+    @GET("/api/v2/pokemon/{id}")
+    fun getPokemon(@Path("id") idDoPokemon: Int) : Call<Pokemon>
 }
